34e2088b4e9e84a76111bddb8285a490af2f1217
wolam <wilhelm@estudiantec.cr>
2020-07-06 20:22:08 -0600

    Poniendo el de gui

--- suma.py
+++ suma.py
@@ -4,10 +4,3 @@ def sumar(a,b):
 	return b + a
 
 print sumar(5,5)
-=======
-def realizar_suma(a,b):
-	
-	return a*1+b*1
-
-print realizar_suma(5,5)
->>>>>>> fef42561b1a344f4778d8a0f028ef69ac747ff86
