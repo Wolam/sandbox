fef42561b1a344f4778d8a0f028ef69ac747ff86
Carstens Wilhelm <53957767+Wolam@users.noreply.github.com>
2020-07-06 20:14:40 -0600

    sumas dentro gui

--- suma.py
+++ suma.py
@@ -1,6 +1,6 @@
 #Funcion que recibe dos números y retorna la suma de los mismos
-def suma(a,b):
+def realizar_suma(a,b):
 	
-	return a + b
+	return a*1+b*1
 
-print suma(5,5)
+print realizar_suma(5,5)
